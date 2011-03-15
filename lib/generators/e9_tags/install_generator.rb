dd2e0e773486a5e66e7023b37a854a17b97981b3
Travis <numbers1311407@gmail.com>
2011-03-15 06:19:10 -0400

    changed module of generator

--- lib/generators/e9_tags/install_generator.rb
+++ lib/generators/e9_tags/install_generator.rb
@@ -1,9 +1,13 @@
-module E9Tags
-  class InstallGenerator < Rails::Generator::NamedBase
-    source_root File.expand_path('../templates', __FILE__)
+require 'rails/generators/named_base'
 
-    def install_javascript
-      copy_file 'tags.js', 'public/javascripts/tags.js'
+module E9Tags
+  module Generators
+    class InstallGenerator < Rails::Generator::NamedBase
+      source_root File.expand_path('../templates', __FILE__)
+
+      def install_javascript
+        copy_file 'tags.js', 'public/javascripts/tags.js'
+      end
     end
   end
 end
