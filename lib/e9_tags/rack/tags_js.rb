e75e6474a675e933cd1438e5de197dfa1265e6f8
Travis <numbers1311407@gmail.com>
2011-03-16 10:39:38 -0400

    changed tags js to not overwrite @params...

--- lib/e9_tags/rack/tags_js.rb
+++ lib/e9_tags/rack/tags_js.rb
@@ -2,11 +2,8 @@ module E9Tags::Rack
   class TagsJs
     def self.call(env)
       if env["PATH_INFO"] =~ /^\/js\/tags/
-        @params = Rack::Request.new(env).params
-
         tags = ::Tagging.joins(:tag).order('tags.name').group_by(&:context).to_json
-
-        js = "window.e9=window.e9||{};window.e9.tags=#{tags};"
+        js   = "window.e9=window.e9||{};window.e9.tags=#{tags};"
 
         [200, {"Content-Type" => "text/javascript", "Cache-Control" => "max-age=3600, must-revalidate"}, [js]]
       else
