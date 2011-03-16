06f9e3fca1f9dd9c2fb71c45535cb18d33d4b06d
Travis <numbers1311407@gmail.com>
2011-03-16 11:04:06 -0400

    fixed apparent route issue caused by passing \'default\' hash to a rack app (it causes url_for(nil) to route to it...)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # TODO fix the fact that we route to apps, then the apps themselves check those paths
-  get '/js/tags' => E9Tags::Rack::TagsJs, :defaults => { :format => 'js' }
+  get '/js/tags' => E9Tags::Rack::TagsJs
   get '/autocomplete/tags' => E9Tags::Rack::TagAutoCompleter
   get '/autocomplete/tag-contexts' => E9Tags::Rack::TagContextAutoCompleter
 end
